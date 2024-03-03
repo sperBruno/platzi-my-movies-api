38bf4241091c745b543735e708ef8835d317bfb8
Bruno Barrios <sperbru797@gmail.com>
2024-03-03 17:17:45 -0400

    🚧 added get movies endpoint

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
+app.title = "Movies API"
 
-@app.get("/")
+movies = [
+    {"id": 1,
+     "title": "Avatar",
+     "overview": "",
+     "year": "2009",
+     "rating": "7.8",
+     "category": "Action"},
+    {"id": 2,
+     "title": "Avatar",
+     "overview": "",
+     "year": "2009",
+     "rating": "7.8",
+     "category": "Fiction"},
+    {"id": 3,
+     "title": "Avatar",
+     "overview": "",
+     "year": "2009",
+     "rating": "7.8",
+     "category": "Action"},
+]
+
+
+@app.get("/", tags=["Home"])
 def message():
-    return "Hello world!"
+    return HTMLResponse("<h1>Hello world!</h1>")
+
+
+@app.get("/movies", tags=["movies"])
+def get_movies():
+    return movies
+
+
+@app.get("/movies/{id}", tags=["movies"])
+def get_movie(id: int):
+    for movie in movies:
+        if movie["id"] == id:
+            return movie
+    return []
+
+
+@app.get("/movies/", tags=["movies"])
+def get_movie_by_category(category: str):
+    movies_by_categories = list(
+        filter(lambda x: x["category"] == category, movies))
+    return movies_by_categories

89e5c5e834426ee0c93d46a4bca95deb3dd88b56
Bruno Barrios <sperbru797@gmail.com>
2024-03-03 18:25:01 -0400

    🚧 added response types

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from typing import Optional
+from typing import List, Optional
 from fastapi import FastAPI, Body, Path, Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
+
 
 class Movie(BaseModel):
     id: Optional[int]
-    title:str = Field(max_length=15)
-    overview:str
-    year:str
+    title: str = Field(max_length=15)
+    overview: str
+    year: str
     rating: int
     category: str
 
@@ -42,32 +43,32 @@ def message():
     return HTMLResponse("<h1>Hello world!</h1>")
 
 
-@app.get("/movies", tags=["movies"])
-def get_movies():
-    return movies
+@app.get("/movies", tags=["movies"], response_model=List[Movie])
+def get_movies() -> List[Movie]:
+    return JSONResponse(content=movies)
 
 
-@app.get("/movies/{id}", tags=["movies"])
+@app.get("/movies/{id}", tags=["movies"], response_model=Movie)
 def get_movie(id: int):
     for movie in movies:
         if movie["id"] == id:
-            return movie
-    return []
+            return JSONResponse(content=movie)
+    return JSONResponse(content=[])
 
 
 @app.get("/movies/", tags=["movies"])
 def get_movie_by_category(category: str = Query(min_length=5, max_length=15)):
     movies_by_categories = list(
         filter(lambda x: x["category"] == category, movies))
-    return movies_by_categories
+    return JSONResponse(content=movies_by_categories)
 
 
-@app.post("/movies", tags=["movies"])
-def create_movie(movie: Movie):
+@app.post("/movies", tags=["movies"], response_model=dict)
+def create_movie(movie: Movie) -> dict:
     movies.append(
         movie
     )
-    return "movie created"
+    return JSONResponse(content="movie created")
 
 
 @app.put("/movies/{id}", tags=["movies"])
@@ -80,11 +81,11 @@ def create_movie(id: int, movie_to_update: Movie):
             movie["year"] = movie_to_update.year
             movie["rating"] = movie_to_update.rating
             movie["category"] = movie_to_update.category
-            return movie
+            return JSONResponse(content=movie)
 
 
 @app.delete("/movies/{id}", tags=["movies"])
 def delete_movie(id: int):
     movies.remove(get_movie(id))
     print(movies)
-    return "movie deleted"
+    return JSONResponse(content="movie deleted")
